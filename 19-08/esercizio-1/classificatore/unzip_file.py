45766af0289cedfaf0edc1d58c898019ace5af85
tiziano bardini <tiziano.bardini@gmail.com>
2025-08-19 10:52:29 +0200

    refactor(unzip): modifica per far decomprimere l'archivio nel main

--- 19-08/esercizio-1/classificatore/unzip_file.py
+++ 19-08/esercizio-1/classificatore/unzip_file.py
@@ -2,12 +2,12 @@ import zipfile
 import os
 
 def unzip_file(zip_path, extract_to):
+    """
+    Estrae tutti i file da uno zip nella cartella indicata.
+    """
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
     print(f"File estratti in: {extract_to}")
-
-if __name__ == "__main__":
-    zip_path = 'archive.zip'
     extract_to = 'dataset'
     if not os.path.exists(extract_to):
         os.makedirs(extract_to)
--- 19-08/esercizio-1/main.py
+++ 19-08/esercizio-1/main.py
@@ -1,6 +1,13 @@
 from classificatore.classificatore import leggi_csv, classifica_consumo
+from classificatore.unzip_file import unzip_file
+import os
 
 def main():
+    zip_path = 'archive.zip'
+    extract_to = 'dataset'
+    if not os.path.exists(extract_to):
+        os.makedirs(extract_to)
+    unzip_file(zip_path, extract_to)
     percorso_csv = 'dataset/COMED_hourly.csv'
     df = leggi_csv(percorso_csv)
     # Classifica per tutti i livelli

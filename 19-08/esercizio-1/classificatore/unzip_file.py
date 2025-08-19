ed8f1018a0af07986413cb2f0037991abf431772
tiziano bardini <tiziano.bardini@gmail.com>
2025-08-19 11:01:33 +0200

    refactor(unzip): cambio funzionamento con libreria zipfile

--- 19-08/esercizio-1/classificatore/unzip_file.py
+++ 19-08/esercizio-1/classificatore/unzip_file.py
@@ -4,11 +4,18 @@ import os
 def unzip_file(zip_path, extract_to):
     """
     Estrae tutti i file da uno zip nella cartella indicata.
+
+    Parametri
+    ----------
+    zip_path : str
+        Percorso del file zip da estrarre.
+    extract_to : str
+        Cartella di destinazione dove estrarre i file.
+
+    Returns
+    -------
+    None
     """
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
     print(f"File estratti in: {extract_to}")
-    extract_to = 'dataset'
-    if not os.path.exists(extract_to):
-        os.makedirs(extract_to)
-    unzip_file(zip_path, extract_to)
